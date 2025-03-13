06750df1d307a22004f549fba96b673140c27816
HanUsePC <wooyeophan@gmail.com>
2025-03-13 22:42:06 +0900

    il 써본거

---
+++ UsefulValues/Program.cs
@@ -0,0 +1,5 @@
+﻿public class UsefulValues
+{
+    public static readonly int StartValue = 20;
+    public const int EndValue = 25;
+}
--- item2/Program.cs
+++ item2/Program.cs
@@ -1,14 +1,28 @@
 ﻿public class Program
 {
+    // public static void Main()
+    // {
+    //     var home = new Home();
+
+    //     System.Console.WriteLine($"Current Cozyness: {Home.Cozyness}");
+    //     System.Console.WriteLine($"Current Warmness: {home.Warmness}");
+    //     System.Console.WriteLine($"Current Warmness: {Home.StaticWarmness}");
+    // }
+
+    // public class Home
+    // {
+    //     public const int Cozyness = 11;
+
+    //     public readonly int Warmness = 7;
+    //     public static readonly int StaticWarmness = 7;
+    // }
+
+
     public static void Main()
     {
-        System.Console.WriteLine($"Current Cozyness: {Home.Cozyness}");
-    }
-
-
-
-    public class Home
-    {
-        public const int Cozyness = 11;
+        for (int i = UsefulValues.StartValue; i <= UsefulValues.EndValue; ++i)
+        {
+            System.Console.WriteLine($"Very Useful value: {i}");
+        }
     }
 }
