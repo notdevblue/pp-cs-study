2006afee639f2695917506b0dc9581bc572ee1d5
HanUsePC <wooyeophan@gmail.com>
2025-03-13 22:19:09 +0900

    최초커밋

---
+++ item2/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        System.Console.WriteLine($"Current Cozyness: {Home.Cozyness}");
+    }
+
+
+
+    public class Home
+    {
+        public const int Cozyness = 11;
+    }
+}
